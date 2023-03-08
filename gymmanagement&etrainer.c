835bd2bfa54d8f8d6db9bb10249d75fa9b614ad8
AnshRehan04 <118544177+AnshRehan04@users.noreply.github.com>
2023-03-08 17:14:43 +0530

    Update gymmanagement&etrainer.c

--- gymmanagement&etrainer.c
+++ gymmanagement&etrainer.c
@@ -10,3 +10,17 @@ struct Member{
   float height;
   float bmi;
 };
+struct Gym{
+  int num_members;
+  struct Members members[Max_Members];
+};
+void print_member(struct Member member) {
+    printf("Name: %s\n", member.name);
+    printf("Age: %d\n", member.age);
+    printf("Gender: %c\n", member.gender);
+    printf("Membership Type: %s\n", member.membership_type);
+    printf("Weight: %.2f kg\n", member.weight);
+    printf("Height: %.2f cm\n", member.height);
+    printf("BMI: %.2f\n", member.bmi);
+    printf("\n");
+}
